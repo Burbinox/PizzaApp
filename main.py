54d0308d788817fe39d68ca859049712e57f8344
Burbinox <hubert.grobelny@interia.pl>
2022-03-12 12:43:16 +0100

    connect with db and GET all pizzas

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from bson import json_util, ObjectId
+import json
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.pizza
+collection = db["pizzas"]
+
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+def welcome():
+    return {"Message": "Welcome to rate pizza app!"}
 
+
+@app.get("/pizza")
+def get_all_pizzas():
+    pizzas_sanitized = json.loads(json_util.dumps(list(collection.find({}))))
+    return {"pizzas": pizzas_sanitized}
